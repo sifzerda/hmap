0b13d12dd8db45ff9470071f76e297ac131c4ccc
Troy <tydamon@hotmail.com>
2024-06-11 04:01:45 +1000

    bunch of stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import OmdbContainer from './components/OmdbContainer';
-import Showtimes from './components/Showtimes';
 
 export default function App() {
   return (
     <>
       <OmdbContainer />
-      <Showtimes />
     </>
   );
 }
